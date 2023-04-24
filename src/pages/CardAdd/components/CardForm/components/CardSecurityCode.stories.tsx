628369ba43b9328cedbe1e14ec34198983d11adc
alwayshere <76519867+herekim@users.noreply.github.com>
2023-04-24 20:08:12 +0900

    fix: CardSecurityCode 스토리북 파일 충돌 해결

--- src/pages/CardAdd/components/CardForm/components/CardSecurityCode.stories.tsx
+++ src/pages/CardAdd/components/CardForm/components/CardSecurityCode.stories.tsx
@@ -1,10 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { useRef } from 'react'
-<<<<<<< HEAD:src/pages/CardAdd/components/CardForm/components/CardSecurityCode.stories.tsx
 
 import { useCardInfo } from '@/pages/hooks'
-=======
->>>>>>> 6c3e046 ([클린코드 리액트 2기 김희열] 페이먼츠 미션 Step 2 (#86)):src/pages/card-add/card-form/components/CardSecurityCode.stories.tsx
 
 import CardSecurityCode from './CardSecurityCode'
 
@@ -14,19 +11,15 @@ export default {
 } as ComponentMeta<typeof CardSecurityCode>
 
 const Template: ComponentStory<typeof CardSecurityCode> = () => {
-<<<<<<< HEAD:src/pages/CardAdd/components/CardForm/components/CardSecurityCode.stories.tsx
   const { handleSecurityCode } = useCardInfo()
-=======
->>>>>>> 6c3e046 ([클린코드 리액트 2기 김희열] 페이먼츠 미션 Step 2 (#86)):src/pages/card-add/card-form/components/CardSecurityCode.stories.tsx
+
   const securityCodeRef = useRef<HTMLInputElement>(null)
   return (
     <div className="root">
       <div className="app">
-<<<<<<< HEAD:src/pages/CardAdd/components/CardForm/components/CardSecurityCode.stories.tsx
+
         <CardSecurityCode securityCodeRef={securityCodeRef} handleChange={handleSecurityCode} />
-=======
-        <CardSecurityCode securityCodeRef={securityCodeRef} />
->>>>>>> 6c3e046 ([클린코드 리액트 2기 김희열] 페이먼츠 미션 Step 2 (#86)):src/pages/card-add/card-form/components/CardSecurityCode.stories.tsx
+
       </div>
     </div>
   )
