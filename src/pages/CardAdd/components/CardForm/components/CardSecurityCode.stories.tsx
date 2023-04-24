0b4ebd11f565b5710f16b4e566daa70409ca8b01
alwayshere <76519867+herekim@users.noreply.github.com>
2023-04-24 19:59:45 +0900

    fix: 새로운 Remote 연결 과정에 Step2와   충돌 해결

--- src/pages/CardAdd/components/CardForm/components/CardSecurityCode.stories.tsx
+++ src/pages/CardAdd/components/CardForm/components/CardSecurityCode.stories.tsx
@@ -1,7 +1,10 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { useRef } from 'react'
+<<<<<<< HEAD:src/pages/CardAdd/components/CardForm/components/CardSecurityCode.stories.tsx
 
 import { useCardInfo } from '@/pages/hooks'
+=======
+>>>>>>> 6c3e046 ([클린코드 리액트 2기 김희열] 페이먼츠 미션 Step 2 (#86)):src/pages/card-add/card-form/components/CardSecurityCode.stories.tsx
 
 import CardSecurityCode from './CardSecurityCode'
 
@@ -11,12 +14,19 @@ export default {
 } as ComponentMeta<typeof CardSecurityCode>
 
 const Template: ComponentStory<typeof CardSecurityCode> = () => {
+<<<<<<< HEAD:src/pages/CardAdd/components/CardForm/components/CardSecurityCode.stories.tsx
   const { handleSecurityCode } = useCardInfo()
+=======
+>>>>>>> 6c3e046 ([클린코드 리액트 2기 김희열] 페이먼츠 미션 Step 2 (#86)):src/pages/card-add/card-form/components/CardSecurityCode.stories.tsx
   const securityCodeRef = useRef<HTMLInputElement>(null)
   return (
     <div className="root">
       <div className="app">
+<<<<<<< HEAD:src/pages/CardAdd/components/CardForm/components/CardSecurityCode.stories.tsx
         <CardSecurityCode securityCodeRef={securityCodeRef} handleChange={handleSecurityCode} />
+=======
+        <CardSecurityCode securityCodeRef={securityCodeRef} />
+>>>>>>> 6c3e046 ([클린코드 리액트 2기 김희열] 페이먼츠 미션 Step 2 (#86)):src/pages/card-add/card-form/components/CardSecurityCode.stories.tsx
       </div>
     </div>
   )
