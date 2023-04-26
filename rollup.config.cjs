5da350fbede864dd48df325d59edab9e6e7ca1ab
alwayshere <76519867+herekim@users.noreply.github.com>
2023-04-27 01:15:23 +0900

    add: Payssion 이름으로 내보내기 추가

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -11,7 +11,7 @@ const { terser } = require('rollup-plugin-terser')
 const typescript = require('rollup-plugin-typescript2')
 
 module.exports = {
-  input: 'src/index.tsx',
+  input: 'src',
   output: [
     {
       file: 'dist/index.js',
