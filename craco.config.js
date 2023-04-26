4a2c68204cb5bd6a9bcac4a32051021b93a088e0
alwayshere <76519867+herekim@users.noreply.github.com>
2023-04-27 00:57:19 +0900

    add: Rollup으로 라이브러리를 위한 빌드파일 생성

--- craco.config.js
+++ craco.config.js
@@ -6,25 +6,5 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
     },
-    configure: (webpackConfig, { env, paths }) => {
-      const isEnvDevelopment = env === 'development'
-
-      // Entry points 설정
-      webpackConfig.entry = {
-        main: [isEnvDevelopment && require.resolve('react-dev-utils/webpackHotDevClient'), paths.appIndexJs].filter(
-          Boolean,
-        ),
-      }
-
-      // Output 설정
-      webpackConfig.output = {
-        ...webpackConfig.output,
-        filename: 'static/js/[name].js',
-        chunkFilename: 'static/js/[name].chunk.js',
-        libraryTarget: 'umd', // 이 부분을 추가하여 UMD를 지원합니다.
-      }
-
-      return webpackConfig
-    },
   },
 }
---
+++ rollup.config.cjs
@@ -0,0 +1,75 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+const { useContext } = require('react')
+const alias = require('rollup-plugin-alias')
+const commonjs = require('rollup-plugin-commonjs')
+const json = require('rollup-plugin-json')
+const resolve = require('rollup-plugin-node-resolve')
+const peerDepsExternal = require('rollup-plugin-peer-deps-external')
+const postcss = require('rollup-plugin-postcss')
+const { terser } = require('rollup-plugin-terser')
+const typescript = require('rollup-plugin-typescript2')
+
+module.exports = {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      exports: 'named',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      exports: 'named',
+    },
+  ],
+  plugins: [
+    alias({
+      '@': path.resolve(__dirname, 'src/'),
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      // left-hand side can be an absolute path, a path
+      // relative to the current directory, or the name
+      // of a module in node_modules
+      namedExports: {
+        'node_modules/react/index.js': [
+          'cloneElement',
+          'createContext',
+          'Component',
+          'createElement',
+          'useContext',
+          'jsx',
+          'useEffect',
+          'useRef',
+          'useState',
+          'useMemo',
+          'useCallback',
+          'useReducer',
+          'useLayoutEffect',
+          'useImperativeHandle',
+          'forwardRef',
+          'memo',
+          'Children',
+          'isValidElement',
+          'useDebugValue',
+          'Fragment',
+        ],
+        'node_modules/react/jsx-runtime.js': ['jsx', 'Fragment', 'jsxs'],
+        'node_modules/react-dom/index.js': ['render', 'hydrate', 'flushSync', 'createPortal'],
+        'node_modules/react-is/index.js': ['isElement', 'isValidElementType', 'ForwardRef'],
+      },
+    }),
+    peerDepsExternal(),
+    resolve(),
+    typescript(),
+    json(),
+    postcss({
+      extract: true,
+    }),
+
+    terser(),
+  ],
+}
