7f5d48b9c26f6b8b044cf431b517a63ecbfc55d7
alwayshere <76519867+herekim@users.noreply.github.com>
2023-04-28 20:39:05 +0900

    modify: rollup config input 루트 수정

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -11,7 +11,7 @@ const { terser } = require('rollup-plugin-terser')
 const typescript = require('rollup-plugin-typescript2')
 
 module.exports = {
-  input: 'src/index.ts',
+  input: 'src/index.tsx',
   output: [
     {
       file: 'dist/index.js',
