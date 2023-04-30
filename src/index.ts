a2a943d132f0504cb122e64f403c71d5bcfcd6fe
alwayshere <76519867+herekim@users.noreply.github.com>
2023-05-01 00:20:52 +0900

    modify: index.ts 필요없는 코드 주석처리

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ export { default as Payssion } from '@/Payssion'
 export { usePayssion } from '@/hooks'
 export { PayssionProvider } from '@/providers'
 
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+// import React from 'react'
+// import { createRoot } from 'react-dom/client'
 
-import App from '@/Payssion'
+// import App from '@/Payssion'
 
-const root = createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// const root = createRoot(document.getElementById('root') as HTMLElement)
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
